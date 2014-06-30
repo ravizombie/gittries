579d19218f91deb034146dd93707e772f81b7b24
Ravi M H <rhireman@cisco.com>
2014-06-30 13:16:28 -0700

    did before committing staged changes

--- buff.c
+++ buff.c
@@ -6,7 +6,10 @@
 /* 
     1. added comment here above main to check git \\
     2. adding this to check amend option to the earlier commit
+    3. added to check the diffs commands:
 */
+
+
 int main(int argc, char* argv[])
 {
 
